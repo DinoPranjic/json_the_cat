eda91916260104396d01ebd6a595174c16e5a507
DinoPranjic <dinopranjic92@gmail.com>
2022-02-23 23:02:58 +0000

    refactored function to handle callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,25 @@
 const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      if (body === '[]') {
+        callback('That breed does not exist', null);
+      } else {
+        const data = JSON.parse(body);
+        callback(null, data[0]["description"]);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+/* OLD CODE
 const args = process.argv.splice(2);
 const query = args[0];
-
-
 request('https://api.thecatapi.com/v1/breeds/search?q=' + query, (error, response, body) => {
 
   if (error) {
@@ -17,3 +34,4 @@ request('https://api.thecatapi.com/v1/breeds/search?q=' + query, (error, respons
   }
 
 });
+*/
